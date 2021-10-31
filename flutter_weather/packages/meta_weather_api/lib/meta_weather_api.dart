de1892df6002a4295f75d11a14933fabc89b0e81
sridhar vempati <sridhar.vempati@aa.com>
2021-10-30 19:02:01 -0500

    commit2

---
+++ flutter_weather/packages/meta_weather_api/lib/meta_weather_api.dart
@@ -0,0 +1,10 @@
+library meta_weather_api;
+
+export 'src/meta_weather_api_client.dart';
+export 'src/models/models.dart';
+
+/// A Calculator.
+class Calculator {
+  /// Returns [value] plus 1.
+  int addOne(int value) => value + 1;
+}
---
+++ flutter_weather/packages/weather_repository/lib/weather_repository.dart
@@ -0,0 +1,12 @@
+// library weather_repository;
+
+// /// A Calculator.
+// class Calculator {
+//   /// Returns [value] plus 1.
+//   int addOne(int value) => value + 1;
+// }
+
+library weather_repository;
+
+export 'src/models/models.dart';
+export 'src/weather_repository.dart';
